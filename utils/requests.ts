2550772e244faabb5a359def61a4701f5c60dff0
chimiWangchukWangdi <chimiwangchukw@gmail.com>
2023-02-10 12:48:29 +0600

    env.local

--- utils/requests.ts
+++ utils/requests.ts
@@ -1,4 +1,4 @@
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY = "84c27ba042f1c7aac1fd4c83065b4612";
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const requests = {
