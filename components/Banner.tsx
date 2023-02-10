089a3fe10362958794c6b584be830f7b32efbc64
chimiWangchukWangdi <chimiwangchukw@gmail.com>
2023-02-10 12:43:51 +0600

    alt in Image

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -25,6 +25,7 @@ function Banner({ netflixOriginals }: Props) {
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           layout="fill"
           objectFit="cover"
+          alt=" "
         />
       </div>
 
